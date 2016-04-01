c0331c3485c40f0a6c5b78db2dc9abf4ed0461ac
Jackson Cafazzo <doctorjuno@gmail.com>
2016-04-01 08:36:41 -0700

    add new question, empty components for question, question-style

---
+++ app/components/new-question.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    save1() {
+      var params = {
+        question: this.get('question'),
+        author: this.get('q-author'),
+        notes: this.get('q-notes')
+      };
+      this.set('addNewQuestion', false);
+      this.set('question', "");
+      this.set('q-author', "");
+      this.set('q-notes', "");
+      this.sendAction('save2', params);
+    }
+  }
+});
