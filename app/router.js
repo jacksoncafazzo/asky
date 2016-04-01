89b26ecb3a5f4d4491f68651a1e39c23e01d70e3
Jackson Cafazzo <doctorjuno@gmail.com>
2016-04-01 13:17:39 -0700

    add asky image and styling

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('question', { path:'/question/:question_id'});
   this.route('answer');
+  this.route('about');
 });
 
 export default Router;
